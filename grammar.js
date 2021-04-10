d50b588ebbec713b9977e1c93ed8adcee8f1f83e
Tyler Matijevich <tylermatijevich@gmail.com>
2021-04-10 11:51:19 -0500

    Create for statement, Add comment rules and put in extras, New assignment expression, Add javascript separation functions

--- grammar.js
+++ grammar.js
@@ -1,6 +1,12 @@
 module.exports = grammar({
 	name: 'structured_text',
 	
+	extras: $ => [
+		$.inline_comment,
+		$.block_comment,
+		/\s/
+	],
+	
 	word: $ => $.identifier,
 	
 	conflicts: $ => [
@@ -11,7 +17,9 @@ module.exports = grammar({
 		$._definition,
 		$.statement,
 		$._control_statement,
+		$._loop_statement,
 		$._expression,
+		$._literal,
 	],
 	
 	rules: {
@@ -40,13 +48,18 @@ module.exports = grammar({
 		statement: $ => choice(
 			$.assignment,
 			$._control_statement,
+			$._loop_statement,
 			// expression statement
-			// loop statement
 		),
 		
 		_control_statement: $ => choice(
 			$.case_statement,
 			$.if_statement
+		),
+		
+		_loop_statement: $ => choice(
+			$.for_statement,
+			// repeat statement
 		),
 		
 		assignment: $ => seq(
@@ -74,6 +87,15 @@ module.exports = grammar({
 			repeat($.case),
 			optional($.else_case),
 			'END_CASE',
+			optional(';')
+		),
+		
+		for_statement: $ => seq(
+			'FOR',
+			$.for_range,
+			'DO',
+			repeat($.statement),
+			'END_FOR',
 			optional(';')
 		),
 		
@@ -112,16 +134,29 @@ module.exports = grammar({
 			field('UpperLimit', $.number)
 		),
 		
+		for_range: $ => seq(
+			$.expression_assignment,
+			'TO',
+			$._expression,
+			optional(seq('BY', $._expression))
+		),
+		
 		/*
 			Expressions
 		*/
 		
 		_expression: $ => choice(
-			$.literal,
+			$._literal,
 			$.identifier, // (variable)
 			// parenthises expression
 			// unary expression
 			// binary expression
+		),
+		
+		expression_assignment: $ => seq(
+			$.identifier,
+			':=',
+			$._expression
 		),
 		
 		/*
@@ -132,14 +167,40 @@ module.exports = grammar({
 			Literals
 		*/
 		
-		literal: $ => choice(
+		_literal: $ => choice(
 			$.number, 
 			// string
 			// time or date
 		),
 		
+		number: $ => token(/\d+/),
+		
+		/*
+      Comments: Reviewed tree-sitter-javascript\grammar.js and tree-sitter-c\grammar.js
+    */
+    inline_comment: $ => token(seq('//', /.*/)), // token fixes rust backtrace warning
+    
+    block_comment: $ => token(seq( // http://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment/36328890#36328890
+      '(*',
+      // 1. Match 0+ characters other than `*`, followed by 1+ literal `*`: `[^*]*\*+`
+      // 2. 0+ of:
+      // 2a. Not a `*` or `)`
+      // 2b. 0+ non-asterisk
+      // 2c. 1+ asterisk
+      /[^*]*\*+([^*)][^*]*\*+)*/,
+      ')'
+    )),
+		
 		identifier: $ => token(/[a-zA-Z_]\w*/),
 		
-		number: $ => token(/\d+/)
 	}
+	
 });
+
+function commaSep1(rule) {
+  return seq(rule, repeat(seq(',', rule)));
+}
+
+function commaSep(rule) {
+  return optional(commaSep1(rule));
+}
